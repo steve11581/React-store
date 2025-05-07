b288707dd1d4f0a0707b63e32b04024398289279
Enes <steve262115@gmail.com>
2025-05-07 18:11:12 +0300

    clarus-store
    react ile geliştirilmiştir.

---
+++ rc15-16_clarus-store/inclass/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Stats from "../components/Stats";
+import Testimonial from "../components/Testimonial";
+
+const Home = () => {
+  return (
+    <div>
+      <Stats />
+      <Testimonial />
+    </div>
+  );
+};
+
+export default Home;
---
+++ rc15-16_clarus-store/inclass/src/router/AppRouter.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import About from "../pages/About";
+import NotFound from "../pages/NotFound";
+import Navbar from "../components/Navbar";
+import PrivateRouter from "./PrivateRouter";
+import ProductDetail from "../pages/ProductDetail";
+
+const AppRouter = () => {
+  return (
+    //* AuthProvider içinde react-router-dom'a ait yapıları kullanabilmek için index.js'e taşıdık
+    // <BrowserRouter>
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<PrivateRouter />}>
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/dashboard/products" element={<Products />} />
+          <Route path="/dashboard/products/:id" element={<ProductDetail />} />
+          {/* //* Absolute path */}
+          <Route path="/dashboard/about" element={<About />} />
+          {/* //* relative path */}
+          {/* <Route path="about" element={<About />} /> */}
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+    // </BrowserRouter>
+  );
+};
+
+export default AppRouter;
